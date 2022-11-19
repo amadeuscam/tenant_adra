a63fe7ed51c4cc6fca61d6597fb17b8272206658
amadeuscam <amadeuscam@yahoo.es>
2022-11-19 21:55:03 +0100

    fix  static files

--- adra_project/settings/production.py
+++ adra_project/settings/production.py
@@ -6,7 +6,6 @@ DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
-
 DATABASES = {
     "default": {
         "ENGINE": os.getenv("SQL_ENGINE"),
@@ -24,7 +23,6 @@ SENDGRID_ALIMENTOS_TEMPLATE_ID = str(os.getenv("SENDGRID_TEMPLATE_ID"))
 SENDGRID_SANDBOX_MODE_IN_DEBUG = False
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-
 # aws settings
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
@@ -33,10 +31,15 @@ AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 # s3 static settings
-AWS_LOCATION = "static"
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# AWS_LOCATION = "static"
+# STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 sentry_sdk.init(
     dsn=f"{os.environ.get('SENTRY_SDK_URL')}",
